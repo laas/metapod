df8cad1e681d25044af3fbae8d9cc6b4407107ce
Maxime REIS <maxime.reis@gmail.com>
2012-03-16 10:43:44 +0100

    Update performance test for rnea.

--- tests/test_rnea.cc
+++ tests/test_rnea.cc
@@ -31,14 +31,6 @@ using namespace simplehumanoid;
 
 BOOST_AUTO_TEST_CASE (test_rnea)
 {
-  // Perf test
-# ifdef METAPOD_PERF_TEST
-  long TICKS_PER_SECOND = 1000000;
-  struct timeval tv_start, tv_stop;
-  struct timezone tz;
-  int N=10000;
-# endif
-
   // Set configuration vectors (q, dq, ddq) to reference values.
   vectorN q(Robot::nbDof), dq(Robot::nbDof), ddq(Robot::nbDof);
 
@@ -55,19 +47,7 @@ BOOST_AUTO_TEST_CASE (test_rnea)
   ddqconf.close();
 
   // Apply the RNEA to the metapod multibody and print the result in a log file.
-# ifdef METAPOD_PERF_TEST
-  ::gettimeofday(&tv_start, &tz);
-  for(int i=0; i<N; i++)
-  {
-# endif
-    // Run the RNEA to compute the torque
-    rnea<Robot::Tree>(q, dq, ddq);
-# ifdef METAPOD_PERF_TEST
-  }
-  ::gettimeofday(&tv_stop, &tz);
-  long time_usec = ( tv_stop.tv_sec - tv_start.tv_sec ) * TICKS_PER_SECOND + ( tv_stop.tv_usec - tv_start.tv_usec );
-  std::cerr << "RNEA execution time = " << (double)time_usec/(double)N << "µs" << std::endl;
-# endif
+  rnea<Robot::Tree>(q, dq, ddq);
   std::ofstream log("rnea.log", std::ofstream::out);
   printTorques<Robot::Tree>(log);
   log.close();
@@ -87,7 +67,8 @@ BOOST_AUTO_TEST_CASE (test_rnea)
       if(y != 0)
       {
         BOOST_CHECK(compareDouble(x,y,1e-3)
-                 && "Difference found in log and reference files (rnea.log and rnea.ref).");
+                 && "Difference found in log and reference files\
+                    (rnea.log and rnea.ref).");
         if(!compareDouble(x,y,1e-3))
           std::cerr << jointname << "\n\t" << x << "\n\t" << y << std::endl;
       }
@@ -104,6 +85,43 @@ BOOST_AUTO_TEST_CASE (test_rnea)
   }
   result_log.close();
   ref_log.close();
+
+  // Perf test
+# ifdef METAPOD_PERF_TEST
+  long TICKS_PER_SECOND = 1e6;
+  struct timeval tv_start, tv_stop;
+  int N1 = 1000;
+  int N2 = 100;
+
+  std::ofstream perf_log("rnea_perf.log", std::ofstream::out);
+
+  long time_usec = 0;
+  long inner_loop_time;
+  // Outer loop : generate random configuration
+  for(int i=0; i<N1; i++)
+  {
+    q = vectorN::Random(Robot::nbDof);
+    dq = vectorN::Random(Robot::nbDof);
+    ddq = vectorN::Random(Robot::nbDof);
+    ::gettimeofday(&tv_start, NULL);
+    // Inner loop : The timer precision is 1µs, which is not high enough to
+    // give proper result on a single iteration 
+    for(int k=0; k<N2; k++)
+      rnea<Robot::Tree>(q, dq, ddq);
+    ::gettimeofday(&tv_stop, NULL);
+    
+    inner_loop_time = ( tv_stop.tv_sec - tv_start.tv_sec ) * TICKS_PER_SECOND
+               + ( tv_stop.tv_usec - tv_start.tv_usec );
+    time_usec += inner_loop_time;
+    // Log inner_loop_time to allow for statistical computations 
+    perf_log << (double)inner_loop_time/(double)N2 << std::endl;
+  }
+  // Output global average execution time
+  std::cout
+    << "RNEA execution time = " << (double)time_usec/(double)(N1*N2) << "µs"
+    << std::endl;
+  perf_log.close();
+# endif
 }
 
 #endif
