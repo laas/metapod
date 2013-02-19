acb12f474e5e559d9ada20efa0bd22f474fdc882
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2013-02-19 11:31:09 +0100

    Revert "Split benchmark models in separate files to ease performance analysis."
    This reverts commit 11acadf014b0f783ca1cc8f461989025773cb2ee.

--- benchmark/benchmark.cc
+++ benchmark/benchmark.cc
@@ -26,7 +26,13 @@
 #ifdef WITH_SIMPLE_HUMANOID
 # include "metapod/models/simple_humanoid/simple_humanoid.hh"
 #endif
-
+#include "models/sample_1/sample_1.hh"
+#if 0
+#include "models/sample_2/sample_2.hh"
+#include "models/sample_3/sample_3.hh"
+#include "models/sample_4/sample_4.hh"
+#endif
+//#include "models/sample_5/sample_5.hh"
 
 #include "benchmark.hh"
 using namespace metapod::benchmark;
@@ -35,6 +41,12 @@ int main()
 {
 #ifdef WITH_SIMPLE_HUMANOID
   BENCHMARK(metapod::simple_humanoid);
+#endif
+  BENCHMARK(metapod::sample_1);
+#if 0
+  BENCHMARK(metapod::sample_2);
+  BENCHMARK(metapod::sample_3);
+  BENCHMARK(metapod::sample_4);
 #endif
   //BENCHMARK(metapod::sample_5);
 }
