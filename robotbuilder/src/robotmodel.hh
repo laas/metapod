282f3f982c8100fa62c62914a5c43b21f1220180
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2013-02-21 15:15:14 +0100

    robotbuilder: remove Xt and sXp types from model
    since they are not used anymore

--- robotbuilder/src/robotmodel.hh
+++ robotbuilder/src/robotmodel.hh
@@ -40,8 +40,6 @@ public:
   Eigen::Vector3d joint_axis_;
   int dof_index_;
   std::vector<int> child_id_; // children
-  std::string xt_type_;
-  std::string sxp_type_;
 
   Link(
     int id,
@@ -77,17 +75,6 @@ public:
   int child_id(int link_id, unsigned int rank) const;
   void add_link(const Link& link);
   int find_link_by_body_name(const std::string& body_name) const;
-    /** \brief Specify Xt type. */
-  void set_link_xt_type(int link_id,
-                        const std::string &xt_type);
-
-  const std::string &xt_type(int link_id) const;
-
-  /** \brief Specify sXp type.*/
-  void set_link_sxp_type(int link_id,
-                         const std::string &sxp_type);
-  const std::string &sxp_type(int link_id) const;
-
   /** @} */
 
 private:
