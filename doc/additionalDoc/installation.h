440cf66acd2b375fb5d951f03bce8053c8ccc62b
Olivier Stasse <olivier.stasse@gmail.com>
2013-03-06 05:35:51 +0100

    Installation documentation.

---
+++ doc/additionalDoc/installation.h
@@ -0,0 +1,70 @@
+/* Copyright 2011, 2012,
+*
+* Olivier STASSE, LAAS/CNRS
+*
+* This file is part of metapod.
+* metapod is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* metapod is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Lesser Public License for more details.
+* You should have received a copy of the GNU Lesser General Public License
+* along with metapod.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Creation: 02/03/2013
+*/
+
+/**
+\page installation Installation
+
+\subsection Dependencies
+This package depends upon :
+<ul>
+   <li> System tools:
+     <ul>
+       <li> CMake (>=2.6) </li>
+       <li> pkg-config </li>
+       <li> usual compilation tools </li>
+     </ul>
+   </li>
+   <li> Libraries:
+     <ul>
+       <li> Eigen (>=3.0.0) </li>
+       <li> Boost (1.40.0 and 1.46.0) Boost Test is used in the test suite </li>
+       <li> Optionally liburdf, as provided by ROS </li>
+     </ul>
+   </li>
+</ul>
+
+If you want to build the urdf converter, you'll need to install ROS too, and to run
+
+source /opt/ros/fuerte/setup.bash
+
+before running cmake.
+
+There is no currently no binary package.
+
+\subsection getting_the_code Getting the code
+To compile the code you can clone it from github:
+\verbatim
+git clone https://github.com/laas/metapod.git
+\endverbatim
+
+\subsection building Building
+To compile this package, it is recommended to create a separate build directory:
+\verbatim
+mkdir _build
+cd _build
+cmake -DBUILD_METAPODFROMURDF=OFF
+make install
+\endverbatim
+
+Please note that CMake produces a CMakeCache.txt file which should be deleted to reconfigure a package from scratch.
+
+
+*/
+
--- doc/additionalDoc/package.h
+++ doc/additionalDoc/package.h
@@ -24,11 +24,28 @@
 This is the documentation of Metapod, a template based library for efficient dynamic computation.
 Metapod is targeted for complex redundant mechanical systems such as humanoid robots.
 
-\section Introduction
+\section main_introduction Introduction
 
 The metapod package aims at providing an efficient template based C++ implementation of the rigid body dynamics algorithms described by Roy Featherstone in his book "Rigid Body Dynamics". The goal is to include this software in high-frequency control loop up to 1 Khz.
 
-\section Installation
-This package depends upon boost and Eigen3.
+Metapod provides the support for the two following algorithms:
+<ul>
+  <li> Composite Rigid Body Algorithm (CRBA) </li>
+  <li> Recursive Newton Euler Algorithm (RNEA) </li>
+</ul>
 
+Metapod may optionally create efficient C++ robot models based on their URDF file.
+It relies on the URDF parser provided by ROS.
+
+\section main_installation Installation
+
+The installation procedure is currently only available by compiling the source code.
+See \ref installation for further explanations.
+
+\section Authors
+Maxime Reis
+Olivier Stasse
+Sébastion Barthélemy
+Antonio El Khoury
 */
+
