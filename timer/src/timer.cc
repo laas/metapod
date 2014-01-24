a8e4b037014e64be1c2035d6429cdf6f01afb8a6
Olivier Stasse <olivier.stasse@gmail.com>
2014-01-24 17:13:04 +0100

    Fix test for boost 1.48 when using timer.
    Correct modification which was not included in the previous commit.

--- timer/src/timer.cc
+++ timer/src/timer.cc
@@ -31,7 +31,7 @@
 #include <cassert>
 #include <boost/version.hpp>
 // check the version in case the cmake boost timer detection is flawed
-#if defined(WITH_BOOST_TIMER) && BOOST_VERSION <= 104800
+#if defined(WITH_BOOST_TIMER) && BOOST_VERSION < 104800
 # undef WITH_BOOST_TIMER
 #endif
 #ifdef WITH_BOOST_TIMER
