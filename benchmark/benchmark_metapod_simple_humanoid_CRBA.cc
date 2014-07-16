f9cc7fb5c6ee0e70dbebde42ac78d634fa707c07
Olivier Stasse <olivier.stasse@gmail.com>
2014-07-16 11:54:03 +0200

    Add benchmarks more coherent to standard tests.
    The benchmark developed previously are mandling
    several things:
    - First they are mixing methods calls to the pure computation
    - Second they are not creating new instances of problems at each
    iteration. This bias the computation.
    
    We keep previous benchmarks to keep references with previous
    tests.

---
+++ benchmark/benchmark_metapod_simple_humanoid_CRBA.cc
@@ -0,0 +1,26 @@
+// Copyright 2014
+//
+// Olivier Stasse (LAAS, CNRS)
+//
+// This file is part of metapod.
+// metapod is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// metapod is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License
+// along with metapod.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * This file run performance tests on metapods algorithms, on a sample model
+ */
+#include <benchmark_metapod_simple_humanoid_util.hh>
+
+int main()
+{
+  test<BMSH_CRBA>::run();
+}
